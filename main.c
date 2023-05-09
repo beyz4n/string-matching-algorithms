57f69ea21eaf6f8d2bc8adc4ff8e67b466f80576
beyz4n <95589101+beyz4n@users.noreply.github.com>
2023-05-09 21:41:37 +0300

    Half done bugs

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include <string.h>
+#include <stdio.h>
 // occurence counter for horspool's algorithm
 int horspoolsOccurence;
 // this method creates a shift table for a given pattern
-int * createShiftTable(char pattern[]){
+void createShiftTable(int *shiftTable, char pattern[]){
     // maximum 500 unique characters, they all have a position here, 0 if no shift can be given using pattern
-    int shiftTable[500] = {0};
     // we shouldn't include the last character so -1 - 1
     int currentPt = strlen(pattern) - 2;
     // while we don't exceed the pattern starting from the right to left
@@ -18,33 +18,41 @@ int * createShiftTable(char pattern[]){
 }
 // this method takes input arguments as text and pattern as a char array
 void horspools(char text[],char pattern[]){
+    int patternLen = strlen(pattern);
     // in Horspool's Algorithm we start from the pattern's right hand side and align it
-    int currentPt = strlen(pattern) - 1;
+    int currentPt = patternLen - 1;
     // create the shift table here
-    int * shiftTable = createShiftTable(pattern);
+    // TODO: Change the 500 to 128
+    int shiftTable[128] = {0};
+    createShiftTable(shiftTable, pattern);
     // while we don't exceeed the text
     while(currentPt < strlen(text)){
         // checking the pattern and text's character is identical or not
-        if(text[currentPt] == pattern[currentPt % strlen(pattern)]){
+        if(text[currentPt] == pattern[currentPt % patternLen]){
             // if it is the pattern's last element (if  the pattern index is 0)
-            if(currentPt % strlen(pattern) == 0){
+            if(currentPt % patternLen == 0){
                 // then we increment the occurence counter
                 horspoolsOccurence++;
+                //mark
+                currentPt += patternLen + 1;
             }
             // whether if there's a total match or a partial match we increment the current pointer
-            currentPt++;
+            currentPt--;
         }
         // if it doesn't match then we look at the shift table that we created for this pattern and shift accordingly
         else{
             // find the shift amount using shift table and the text's mismatched character, if it is not zero then we can use that shift amount
-            int shift = shiftTable[text[currentPt]] != 0 ? shiftTable[text[currentPt]] : strlen(pattern);
-            currentPt += shift;
+            int shift = shiftTable[text[currentPt+1]] != 0 ? shiftTable[text[currentPt]] : patternLen;
+            currentPt += (shift != 6 ? (shift + (patternLen - (currentPt % patternLen) + 1)) : shift);
         }
     }
 
 }
 int main(){
-
-    
+    //horspools("BARD LOVED BANANAS", "BAOBAB");
+    horspools("GCATCGCAGAGAGTATACAGTACG", "GCAGAGAG");
+    //horspools("Hello we are trynaingining somethingnaingining.", "naingining");
+    //horspools("HelloHelloHello", "Hello");
+    printf("%d", horspoolsOccurence);
     return 1;
 }
