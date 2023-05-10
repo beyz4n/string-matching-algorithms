7f78e5b9b105546b3a0799faf640089aea601c7e
Despance <despance1927@hotmail.com>
2023-05-10 12:36:19 +0300

    sorry to delete main

--- main.c
+++ main.c
@@ -80,6 +80,35 @@ int main(){
     //horspools("HelloHelloHello", "Hello");
     char pattern[250];
     char filePath[250];
+
+    
+    printf("enter the pattern to be searched: ");
+    gets(pattern);
+    printf("\nenter the html file name: ");
+    gets(filePath);
+    
+    FILE *file = fopen(filePath,"r"); 
+
+     if(file == NULL){
+        printf("input file could not be found");
+        exit(1);
+    }
+
+    int arraySize = strlen(pattern)*30;
+    if (arraySize >= 500){
+        arraySize = 500;
+    }
+
+    char input[arraySize];
+    while(!feof(file)){
+        
+        for(int i = 0 ; !feof(file) && (i<arraySize) ; i++){
+            fscanf(file, "%c", input[i]);
+        }
+        // call functions
+    }
+    printf("%d", horspoolsOccurence);
+
     printf("brute force test ocurrance: %d", bruteForce("GCATCGCAGAGAGTATACAGTACG","GCAGAGAG",NULL));
     
     return 1;
